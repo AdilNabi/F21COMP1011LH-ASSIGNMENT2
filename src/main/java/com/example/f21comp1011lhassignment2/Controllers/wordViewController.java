0aa796a0cf8c21b8f175f0143e57dfeca81df101
AdilNabi <77406332+AdilNabi@users.noreply.github.com>
2021-12-02 18:16:47 +0500

    First Screen consisting of all elements built and scene works normally.

--- src/main/java/com/example/f21comp1011lhassignment2/Controllers/wordViewController.java
+++ src/main/java/com/example/f21comp1011lhassignment2/Controllers/wordViewController.java
@@ -4,11 +4,10 @@ import com.example.f21comp1011lhassignment2.Models.DictionaryResponse;
 import com.example.f21comp1011lhassignment2.Utilities.APIUtility;
 import com.example.f21comp1011lhassignment2.Models.*;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
 import javafx.scene.text.TextFlow;
 
 import java.lang.reflect.Array;
@@ -17,7 +16,6 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 public class wordViewController implements Initializable{
-
 
     @FXML
     private Label wordText;
@@ -29,7 +27,40 @@ public class wordViewController implements Initializable{
     private Label originText;
 
     @FXML
-    private ListView<String> listView;
+    private ListView<String> synlistView;
+
+    @FXML
+    private ListView<String> antlistView;
+
+    @FXML
+    private Label definitionText;
+
+    @FXML
+    private Label exampleLabel;
+
+
+    @FXML
+    private ComboBox<String> meaningBox;
+
+    @FXML
+    private ComboBox<String> defBox;
+
+    @FXML
+    private TextField searchText;
+
+    @FXML
+    private Button searchButton;
+
+    private int selection;
+    private int finalPick;
+
+    @FXML
+    void getSearchResult(ActionEvent event)
+    {
+        wordText.setText("BUTTON WORKS");
+    }
+
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -40,17 +71,128 @@ public class wordViewController implements Initializable{
         phoneticText.setText(result[0].getPhonetic());
         originText.setText(result[0].getOrigin());
 
-        Meaning[] example = result[0].getMeanings();
-
-       Definition[] def = example[0].getDefinitions();
 
 
 
-        List<String> synonms = List.of(def[0].getSynonyms());
+        Meaning[] meanings = result[0].getMeanings();
 
-        for (int i = 0; i < synonms.size(); i++) {
-            listView.getItems().add(synonms.get(i));
+        for (Meaning mean : meanings)
+        {
+            meaningBox.getItems().add(mean.getPartOfSpeech());
         }
+
+
+
+        EventHandler<ActionEvent> event =
+                new EventHandler<ActionEvent>() {
+                    public void handle(ActionEvent e)
+                    {
+                        selection = (meaningBox.getSelectionModel().getSelectedIndex());
+                        defBox.getItems().clear();
+                        definitionText.setText("");
+                        exampleLabel.setText("");
+                        synlistView.getItems().clear();
+                        antlistView.getItems().clear();
+
+                        Definition[] def = meanings[selection].getDefinitions();
+                        for (Definition definition : def)
+                        {
+                            defBox.getItems().add(definition.getDefinition());
+                        }
+
+                        defBox.getSelectionModel().selectFirst();
+
+                        if (finalPick == -1)
+                        {
+                            finalPick = 0;
+                        }
+
+
+
+                    }
+                };
+
+        meaningBox.setOnAction(event);
+
+
+
+        Definition[] def = meanings[selection].getDefinitions();
+
+
+
+
+
+        EventHandler<ActionEvent> event2 =
+                new EventHandler<ActionEvent>() {
+                    public void handle(ActionEvent e)
+                    {
+                        if (finalPick == -1)
+                        {
+                            finalPick = 0;
+                        }
+                        finalPick = (defBox.getSelectionModel().getSelectedIndex());
+                        if (finalPick == -1)
+                        {
+                            finalPick = 0;
+                        }
+                        definitionText.setText("");
+                        exampleLabel.setText("");
+                        synlistView.getItems().clear();
+                        antlistView.getItems().clear();
+
+                        definitionText.setText(def[finalPick].getDefinition());
+                        exampleLabel.setText(def[finalPick].getExample());
+
+
+                        if (def[finalPick].getSynonyms().length > 0)
+                        {
+                            List<String> synonms = List.of(def[finalPick].getSynonyms());
+
+
+                            for (int i = 0; i < synonms.size(); i++) {
+                                synlistView.getItems().add(synonms.get(i));
+                            }
+
+                        }
+
+
+
+
+                        if (def[finalPick].getAntonyms().length > 0)
+                        {
+                            List<String> anynons = List.of(def[finalPick].getAntonyms());
+                            for (int i = 0; i < anynons.size(); i++) {
+                                antlistView.getItems().add(anynons.get(i));
+                            }
+                        }
+
+
+//
+
+
+                    }
+                };
+
+        defBox.setOnAction(event2);
+
+
+//       definitionText.setText(def[finalPick].getDefinition());
+//       exampleLabel.setText(def[finalPick].getExample());
+//
+//
+//        List<String> synonms = List.of(def[finalPick].getSynonyms());
+//
+//        for (int i = 0; i < synonms.size(); i++) {
+//            synlistView.getItems().add(synonms.get(i));
+//        }
+//
+//
+//
+//        List<String> anynons = List.of(def[finalPick].getAntonyms());
+//        for (int i = 0; i < anynons.size(); i++) {
+//            antlistView.getItems().add(anynons.get(i));
+//        }
+
 
     }
 }
